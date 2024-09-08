d2a0eb2c0b099656921b93579eaad6f0f5c31b16
NadiyPro <siroviyn13@gmail.com>
2024-09-08 14:40:28 +0300

    create

--- code_03/index.js
+++ code_03/index.js
@@ -1,3 +1,6 @@
 // пример подключения именнованого модуля
 const m1 = require('./example_module');
 console.log(m1.randomInt());
+//или через деструктуризацию
+const {randomInt} = require('./example_module');
+console.log(randomInt());
